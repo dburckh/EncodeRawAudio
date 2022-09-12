5a95798facdf6d868d12b23c7298229a06d375d5
Dustin <dburckh@gmail.com>
2022-09-12 09:06:38 -0600

    Minor clean up

--- app/src/main/java/com/homesoft/encoderawaudio/EncodeViewModel.kt
+++ app/src/main/java/com/homesoft/encoderawaudio/EncodeViewModel.kt
@@ -5,6 +5,8 @@ import android.app.Application
 import android.media.*
 import android.net.Uri
 import android.os.Environment
+import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import java.io.File
@@ -45,6 +47,9 @@ class EncodeViewModel(app: Application):AndroidViewModel(app) {
         audioRecorderFuture?.cancel(true)
     }
 
+    val recordErrorCode: Int
+        get() = audioRecorderFuture?.get() ?: STATUS_OK
+
     fun startPlay() {
         val state = getRecordState()
         if (state == State.STOPPED || state == State.ERROR) {
@@ -70,10 +75,12 @@ class EncodeViewModel(app: Application):AndroidViewModel(app) {
         }
     }
 
-    val recordErrorCode: Int
-        get() = audioRecorderFuture?.get() ?: 0
-    
+    /**
+     * Process the data from the mic
+     *
+     */
     inner class AudioRecorder:MediaCodec.Callback(), Callable<Int> {
+        //Check is in UI
         @SuppressLint("MissingPermission")
         private val audioRecord = AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, CHANNEL_MASK, FORMAT, BUFFER_SIZE)
         private val mediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_AUDIO_AAC)
@@ -83,13 +90,18 @@ class EncodeViewModel(app: Application):AndroidViewModel(app) {
         private var trackIndex = -1
         private var status = STATUS_INIT_ERROR
 
+        /**
+         * Loop to read raw PCM from AudioReader
+         * @return STATUS_OK or an error code
+         */
+        @WorkerThread
         override fun call(): Int {
             thread = Thread.currentThread()
             if (audioRecord.state == AudioRecord.STATE_UNINITIALIZED) {
                 return status
             }
             val mediaFormat = MediaFormat.createAudioFormat(MediaFormat.MIMETYPE_AUDIO_AAC, SAMPLE_RATE, CHANNELS)
-            mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, 160 * 1024)
+            mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, ENCODER_BIT_RATE)
             try {
                 mediaMuxer = MediaMuxer(mp4File.absolutePath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
                 mediaFormat.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC)
@@ -102,12 +114,14 @@ class EncodeViewModel(app: Application):AndroidViewModel(app) {
                 status = STATUS_OK
                 recordStateData.postValue(State.RUNNING)
                 while (!thread.isInterrupted) {
+                    //Subtle, this will block until we have encoder buffers available
                     val bufferIndex = bufferIndexQueue.take()
                     val byteBuffer = mediaCodec.getInputBuffer(bufferIndex)
                     if (byteBuffer == null) {
                         status = STATUS_NO_ENCODER_INPUT_BUFFER
                         break
                     }
+                    //Per the AudioReader docs we need to leave some buffer for the AudioReader to queue to
                     val readSize = (BUFFER_SIZE / 2).coerceAtMost(byteBuffer.capacity())
 
                     val bytes = audioRecord.read(byteBuffer, readSize)
@@ -134,7 +148,20 @@ class EncodeViewModel(app: Application):AndroidViewModel(app) {
         }
 
         override fun onInputBufferAvailable(p0: MediaCodec, index: Int) {
+            //Queue the buffer indexes so we can
             bufferIndexQueue.offer(index)
+        }
+
+        override fun onOutputFormatChanged(p0: MediaCodec, mediaFormat: MediaFormat) {
+            // Herein lies the magic
+            // MediaCodec adds the undocumented field "csd-0" which contains the
+            // "codec-specific data" mentioned in the MediaMuxer documentation
+            // This is described here https://developer.android.com/reference/android/media/MediaCodec#CSD
+            // It also adds KEY_AAC_PROFILE and KEY_MAX_BIT_RATE (hidden in AOSP)
+            if (trackIndex == -1) {
+                trackIndex = mediaMuxer.addTrack(mediaFormat)
+                mediaMuxer.start()
+            }
         }
 
         override fun onOutputBufferAvailable(mediaCodec: MediaCodec, index: Int, bufferInfo: MediaCodec.BufferInfo) {
@@ -143,34 +170,27 @@ class EncodeViewModel(app: Application):AndroidViewModel(app) {
                 status = STATUS_NO_ENCODER_OUTPUT_BUFFER
                 thread.interrupt()
             } else {
-                // Herein lies the magic
-                // MediaCodec adds the undocumented field "csd-0" which contains the media codec specific data
-                // It also adds KEY_AAC_PROFILE and KEY_MAX_BIT_RATE (hidden in AOSP)
-                if (trackIndex == -1) {
-                    trackIndex = mediaMuxer.addTrack(mediaCodec.outputFormat)
-                    mediaMuxer.start()
-                }
                 mediaMuxer.writeSampleData(trackIndex, byteBuffer, bufferInfo)
                 mediaCodec.releaseOutputBuffer(index, false)
             }
         }
 
-        override fun onError(p0: MediaCodec, p1: MediaCodec.CodecException) {
+        override fun onError(p0: MediaCodec, e: MediaCodec.CodecException) {
+            Log.e(TAG, "Encoder Error", e)
             status = STATUS_ENCODER_ERROR
             thread.interrupt()
         }
 
-        override fun onOutputFormatChanged(p0: MediaCodec, p1: MediaFormat) {
-            //Don't care
-        }
     }
 
     companion object {
+        const val TAG = "AudioEncoder"
         // CD Quality Audio
         const val SAMPLE_RATE = 44100
         const val CHANNEL_MASK = AudioFormat.CHANNEL_IN_STEREO
         val CHANNELS = Integer.bitCount(CHANNEL_MASK)
         const val FORMAT = AudioFormat.ENCODING_PCM_16BIT
+        const val ENCODER_BIT_RATE = 160 * 1024
         const val STATUS_OK = 0
         const val STATUS_INIT_ERROR = -0xff
         const val STATUS_NO_ENCODER_INPUT_BUFFER = -0x100
